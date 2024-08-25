37577b32ba88afc892e2db048ca3d341cee51da4
Connor Friday <connor.friday.cf@gmail.com>
2024-08-25 00:39:14 +0000

    Creating form for new notes

--- notegroups/routes.py
+++ notegroups/routes.py
@@ -1,7 +1,21 @@
-from flask import render_template
+from flask import render_template, request, url_for
 from notegroups import app, db
 from notegroups.models import Note
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
+
+@app.route("/note")
+def note():
+    return render_template("note.html")
+
+@app.route("/new_note", methods=["GET","POST"])
+def new_note():
+    if request.method=="POST":
+        title = request.form.get("title")
+        description = request.form.get("description")
+        date_updated = datetime.utcnow()
+
+        new_note = Note(title=title, description=description, date_updated=date_updated)
+    return render_template("new_note.html")
