fa1f5e88929bed908f4c6c85969d87b43f438833
Connor Friday <connor.friday.cf@gmail.com>
2024-08-26 01:52:28 +0000

    Adding quill api link to base html and new note creation

--- notegroups/routes.py
+++ notegroups/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, url_for
+from flask import render_template, request, redirect, url_for
 from notegroups import app, db
 from notegroups.models import Note
 
@@ -15,7 +15,17 @@ def new_note():
     if request.method=="POST":
         title = request.form.get("title")
         description = request.form.get("description")
+        note_content = request.form.get("note_content")
         date_updated = datetime.utcnow()
 
-        new_note = Note(title=title, description=description, date_updated=date_updated)
+        new_note = Note(
+            title=title,
+            description=description,
+            note_content=note_content,
+            date_updated=date_updated
+        )
+
+        db.session.add(new_note)
+        db.session.commit()
+        return redirect(url_for("home"))
     return render_template("new_note.html")
