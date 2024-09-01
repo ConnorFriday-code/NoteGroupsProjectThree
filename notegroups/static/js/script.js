8dc57eceea71a255c55c3f61cd5503eee111c43d
Connor Friday <connor.friday.cf@gmail.com>
2024-09-01 03:43:28 +0000

    Creating a delete button with working validation

--- notegroups/static/js/script.js
+++ notegroups/static/js/script.js
@@ -7,4 +7,22 @@ $(document).ready(function(){
             $("#dropdown-list").css("display", "none")
         }
     })
+
+
+    $(".delete-button").click(function(){
+        var noteId = $(this).data("note-id")
+        var noteTitle = $(this).data("note-title")
+        $("#delete-check-title").text(noteTitle)
+        $("#confirm-delete").attr("href", "/delete_note/"+noteId)
+
+        $("#delete-check-outer").toggleClass("hidden visible")
+    })
+
+    $("confirm-delete").click(function(){
+        $("#delete-check-outer").toggleClass("hidden visible")
+    })
+
+    $("#cancel-delete").click(function(){
+        $("#delete-check-outer").toggleClass("hidden visible")
+    })
 })
